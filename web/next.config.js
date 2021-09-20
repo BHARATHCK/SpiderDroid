814d2c74d34d1b52a6cee3e14310569d4f66cb99
BHARATH CHANDRA <bharathchandra541@gmail.com>
2021-09-21 00:31:10 +0530

    only server

--- web/next.config.js
+++
@@ -1,6 +0,0 @@
-// next.config.js
-module.exports = {
-    images: {
-        domains: ["via.placeholder.com", "res.cloudinary.com"],
-    },
-}
--- web/src/pages/category.tsx
+++
@@ -1,7 +0,0 @@
-import { withApolloClient } from "../utils/apollo-client";
-
-const Category = () => {
-  return <div>Hello from Category !!</div>;
-};
-
-export default withApolloClient()(Category);
